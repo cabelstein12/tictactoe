73112b2d8a1777e08cfa2bf0798f49364c75d944
cabelstein12 <Caleb.Steinwandt@gmail.com>
2023-11-26 20:33:29 -0700

    <build>[optional_scope]: <finish automatically changing each player after the round. complete checking win status on moves />
    [optional_body]
    
    [optional_footer]

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
 
-let gameBoard = [[0,0,0],[0,0,0],[0,0,0]];
+let gameBoard;
+let k;
+
 
 function resetGame(){
     gameBoard = [[0,0,0],[0,0,0],[0,0,0]];
-}
+    k = 0; 
+}   
+
 
 const Player = [
-    {name: 'player1',
+    {name: 'Player 1',
      value: 1,
     },
-    {name: 'player2',
+    {name: 'Player 2',
      value: 2,
-    }
-]
+    }]
 
-let lastMove;
+
+function alternatePlayer(currentPlayer){
+
+    if(currentPlayer === 0){
+        return k = 1
+    } else {
+        return k = 0
+    }
+}
 
 function placeToken(i, j, k){
     
@@ -22,18 +33,39 @@ function placeToken(i, j, k){
         for(let inner = j; inner < gameBoard[index].length; inner++){
             if(gameBoard[index][inner] != 0){
                 console.log('invalid placement. try again');
-                break;
+                // return `${Player[k].name}'s turn` ;
+            
     
             } else {
                 gameBoard[i][j] = Player[k].value;
                 console.log(`${Player[k].name} played a token`);
-                lastMove = k;
-                break;
-            
+                let testWin =  checkWin(k+1)
+                if(testWin === true){
+                    return `${Player[k].name} Wins!`
+                }else {
+                    k = alternatePlayer(k);
+                }
+                
+            }
+            console.table(gameBoard);  
+            return `${Player[k].name}'s turn` ;
         }
     }
-    console.table(gameBoard);  
-    console.log(`the previous move was by ${Player[lastMove].name}`)
-    return lastMove;           
 }
+
+function checkWin(player){
+    
+    for(let i = 0; i < 3; i++){
+        if(gameBoard[0][i] === player && gameBoard[1][i] === player && gameBoard[2][i] === player || 
+            gameBoard[i][0] === player && gameBoard[i][1] === player && gameBoard[i][2] === player){
+                return  true;
+            }
+
+        }if(gameBoard[0][0] === player && gameBoard[1][1] === player && gameBoard[2][2] === player || 
+            gameBoard[0][2] === player && gameBoard[1][1] === player && gameBoard[2][0] === player){
+            return true;
+        } else {
+            return false;
+        }
 }
+resetGame();
